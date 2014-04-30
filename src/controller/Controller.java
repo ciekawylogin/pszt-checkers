02222f4a95e1bc5ecde94c570d64aac8f78746c9
mkrawczak <michalarturkrawczak@gmail.com>
2014-04-30 20:20:40 +0000

    Dodanie zalążków interfejsów do najważniejszych plików

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,59 @@
 package controller;
 
+import model.Model;
+import view.View;
+import common.events.GameEvent;
+
+import java.util.concurrent.BlockingQueue;;
+
 public class Controller {
+    /// Model 
+    private final Model model;
+     
+    /// Widok 
+    private final View view;
+    
+    /// Kolejka blokująca do przyjmowania zdarzeń od widoku
+    private final BlockingQueue <GameEvent> blocking_queue;
+    
+    /**
+     * Konstruktor sterownika, do wołania w mainie programu
+     * 
+     * @param model Model
+     * @param view Widok
+     * @param blocking_queue Kolejka zdarzeń od widoku
+     */
+    public Controller(final Model model, final View view, final BlockingQueue<GameEvent> blocking_queue) {
+        this.model = model;
+        this.view = view;
+        this.blocking_queue = blocking_queue;
+    }
+    
+    /**
+     * Metoda inicjalizuje program (tj. inicjalizuje sterownik, a następnie, jeśli trzeba, 
+     * woła metody inicjalizujące model i widok), po czym wchodzi w główną pętlę programu.
+     */
+    public void go() {
+    	view.init();
+    	while(true) {
+    		processEvents();
+    	}
+    }
+
+    /**
+     * Pobiera obiekt z kolejki zdarzeń (być może czekając na niego), po czym obsługuje go
+     */
+	private void processEvents() {
+		try {
+			GameEvent event = this.blocking_queue.take();
+			// @TODO: zrob cos z event 
+		}
+		catch(InterruptedException exception)
+		{
+			// nie powinno sie zdarzyć
+			throw new RuntimeException("unexpected exception");
+		}
+	}
 
 }
  
--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,32 @@
 package view;
 
-public class View {
+import java.util.concurrent.BlockingQueue;
+import common.events.GameEvent;
+import common.Mockup;
 
+import java.lang.UnsupportedOperationException;
+
+public class View {
+    private BlockingQueue blocking_queue;
+    
+    public View(BlockingQueue<GameEvent> blocking_queue) {
+    	this.blocking_queue = blocking_queue;
+    }
+
+    /**
+     * Inicjuje widok, np. odpala okno gry (na razie nic nie wyswietla - bezposrednio potem
+     * zostanie zawolana metoda draw)
+     */
+	public void init() {
+		// @TODO write me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	/**
+	 * Wyświetla stan gry na podstawie podanej makiety
+	 */
+	public void draw(Mockup mockup) {
+		// @TODO write me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
