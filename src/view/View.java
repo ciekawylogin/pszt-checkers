c4b6e2513ff8b5b2b3945d4dc44b73c0c2063239
dbielaws <dbielaws@stud.elka.pw.edu.pl>
2014-05-16 16:39:48 +0200

    nowa wersja menu

--- src/view/View.java
+++ src/view/View.java
@@ -13,11 +13,12 @@ import java.util.concurrent.BlockingQueue;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 // tylko do debugu
@@ -36,21 +37,30 @@ public class View extends Application implements Runnable {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    	try {
+    		AnchorPane page = (AnchorPane) FXMLLoader.load(View.class.getResource("menu.fxml"));
+            primaryStage.setScene(new Scene(page));
+            primaryStage.setTitle("Checkers");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    	
+//        primaryStage.setTitle("Hello World!");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(new Scene(root, 300, 250));
+//        primaryStage.show();
     }
 
     /**
