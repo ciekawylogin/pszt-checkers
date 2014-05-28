c456fe76d21660967df41c9be1834986d866484c
mkrawczak <michalarturkrawczak@gmail.com>
2014-05-28 17:14:52 +0200

    Revert "popraweczki"
    This reverts commit f1bc1cec1c189df0fed99600ff6a3f086f6a5b21.

--- src/view/View.java
+++ src/view/View.java
@@ -275,6 +275,7 @@ public class View extends Application implements Runnable {
                     }
                 });
 
+                
                 for(Coordinate xy : mockup.getDeletedCheckers()) {
                     
                     FadeTransition fadeTransition = FadeTransitionBuilder.create()
@@ -290,6 +291,8 @@ public class View extends Application implements Runnable {
                     checkersOnBoard[xy.getX()][xy.getY()] = null;
                 }
                     
+
+                
                 checkersOnBoard[endX][endY] = checkersOnBoard[startX][startY];
                 checkersOnBoard[startX][startY] = null;
             }
